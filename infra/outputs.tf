e184535aa10f5d07e8570d31abe86d2436a1c082
Sarvagna Kumar Rudrangi <145482587+sarva0-0@users.noreply.github.com>
2026-03-31 14:37:42 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,7 @@
 output "master_public_ip" {
-  value = aws_instance.master.public_ip
+  value = aws_eip.master.public_ip
 }
 
 output "worker_public_ips" {
-  value = aws_instance.workers[*].public_ip
+  value = aws_eip.workers[*].public_ip
 }
